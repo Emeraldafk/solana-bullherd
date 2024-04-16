b314f1a6d399fca801d65f054b3a67f1e9110dd6
Emeraldafk <79530189+Emeraldafk@users.noreply.github.com>
2024-04-15 19:07:15 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,9 @@ const candyMachineId = new anchor.web3.PublicKey(
   process.env.REACT_APP_CANDY_MACHINE_ID!
 );
 
-const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
+const network = process.env.devnet as WalletAdapterNetwork;
 
-const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
+const rpcHost = process.env.https://api.devnet.solana.com;
 const connection = new anchor.web3.Connection(rpcHost);
 
 const txTimeout = 30000; // milliseconds (confirm this works for your project)
